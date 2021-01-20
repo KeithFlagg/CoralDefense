1426a3e48e9f7d5b1563adb2f6e811cc6ad16a1c
Lens Desrouleaux <65369396+lrdesrouleaux@users.noreply.github.com>
2021-01-20 10:58:06 -0500

    Update totalarduino.ino
    Changed the main timing loop to run off the triggers specified so every second it reads the Bluetooth module and every 4 seconds it writes to the Bluetooth module rather than relying on delays in the loop. After testing with the phone hoping to make the triggers every 250ms for a read and 2 seconds for data.

--- Arduino/totalarduino.ino
+++ Arduino/totalarduino.ino
@@ -6,22 +6,25 @@
 #define he_pin 25
 #define dht_pin 4
 #define fs_pin 2
-#define sensor_interrupt 0 //
+#define sensor_interrupt 0 
 #define ls_pin 20
 #define led_pin 53
 #define fwfp_pin 22
 //defining device pins
-#define bt Serial1 //HC-05 bluetooth chip \
+#define bt Serial1                       //HC-05 bluetooth chip 
                    //all the information relating to the flow sensors
-float flowrate;
-float volumeml;
-unsigned long totalml;
-unsigned long oldtime;
-volatile byte pulsecount;
-float calibration = 4.5;
-//make a dht sensor object to read humidity and temperature
-dht DHT;
-int bt_error(int); //error handlers
+float flow_rate_ml;                      //keeps track of flow rate
+float flow_volume_ml;                    //keeps track of volume
+unsigned long flow_total_ml;             //keeps track of the total amount of flow
+unsigned long old_flow_time;             //keeps track of time passed for the flow function
+volatile byte flow_sensor_pulse_count;   //keeps track of the pulses the flow sensor has sent
+float flow_sensor_tick_rate = 1;         //flow_sensor_tick_rate on the flow sensor
+const long recieve_time_trigger = 1000;  //triggers after 1000 ms to run code
+const long package_time_trigger = 4000;  //triggers after 4000 ms to run code
+unsigned long recieve_previous_time = 0; //keeps time before the recieve_data code runs
+unsigned long package_previous_time = 0; //keeps time before the package and send code runs
+dht DHT;                                 //make a dht sensor object to read humidity and temperature
+int bt_error(int);                       //error handlers
 int inboard_error(int);
 int get_flow_info();                  //get all the information regarding the  flow
 int package_bundle();                 // gather data for and formate the bublde string return negative if error
@@ -37,9 +40,9 @@ float get_temperature();              //read temperature and return
 void turn_on_fwf_pump();              //turns on the fresh water fill pump
 void turn_off_fwf_pump();             //turns off the freshwater fill pump
 char *convert_float_to_string(float); //converts a float to a string
-void pulseCounter();                  //used to count pulses, keeps time for flowinfo
+void flow_sensor_pulse_counter();     //used to count pulses, keeps time for flowinfo
 char sender[255];                     // string to be formatted and sent over bluetooth
-int receiver = -1;                    //int that gets read from the phone
+int receiver = 0;                     //int that gets read from the phone
 char *arr[5];                         //string array for formatting sender
 void setup()
 {
@@ -51,59 +54,57 @@ void setup()
   pinMode(fs_pin, INPUT);
   Serial.begin(9600);
   bt.begin(9600);
-  flowrate = 0;
-  volumeml = 0;
-  totalml = 0;
-  oldtime = 0;
-  pulsecount = 0;
-  attachInterrupt(sensor_interrupt, pulseCounter, FALLING); //interrupt when voltage falls, and run the function pulseCounter
+  flow_rate_ml = 0;
+  flow_volume_ml = 0;
+  flow_total_ml = 0;
+  old_flow_time = 0;
+  flow_sensor_pulse_count = 0;
+  attachInterrupt(sensor_interrupt, flow_sensor_pulse_counter, FALLING); //interrupt when voltage falls, and run the function pulseCounter
 }
 
 void loop()
 {
   int packagecheck, sendcheck, readcheck;
-  packagecheck = package_bundle();
-  if (packagecheck < 0)
+  unsigned long current_time = millis();
+  //recieve data timer
+  if (current_time - recieve_previous_time >= recieve_time_trigger)
   {
-    Serial.println("Packaging error");
-    Serial1.println("Packaging error");
-  }
-  sendcheck = send_bundle();
-  if (sendcheck < 0)
-  {
-    Serial.println("Sending error");
-    bt.println("Sending error");
-  }
-  delay(1700);
-  readcheck = recieve_data();
-  if (readcheck < 0)
-  {
-    Serial.println("Nothing new read");
-    bt.println("Nothing new read");
-  }
-  if (readcheck > 0) //there is a passed number from phone that is not a error
-  {
-    int stater;
-    stater = state_check(readcheck);
-    if (stater < 0)
+    if ((readcheck = recieve_data()) == 0)
     {
-      inboard_error(stater);
+      Serial.println("----Nothing new read----");
     }
+    if (readcheck < 0)
+    {
+      bt_error(readcheck);
+    }
+    else
+    {
+      state_check(readcheck);
+    }
+    recieve_previous_time = current_time;
   }
-  if (readcheck == 0)
+  //package and send timer
+  if (current_time - package_previous_time >= package_time_trigger)
   {
-    Serial.println("No message sent");
-  }
-  delay(1700);
-}
-void pulseCounter()
+    if ((packagecheck = package_bundle()) >= 0)
+    {
+      Serial.println("----Successful Packaging----");
+      if ((sendcheck = send_bundle()) >= 0)
+      {
+        Serial.println("----Successful Sending----");
+      } //sendcheck if
+    }   //packagecheck if
+    package_previous_time = current_time;
+  } //package timer
+} //eof
+void flow_sensor_pulse_counter()
 {
-  pulsecount++;
+  flow_sensor_pulse_count++;
 }
-char *convert_float_to_string(float n)
+char *convert_float_to_string(float n, int stringlength, int precision)
 {
-  char *mynum = new char[5];
-  dtostrf(n, 3, 2, mynum);
+  char *mynum = new char[15];
+  dtostrf(n, stringlength, precision, mynum);
   return mynum;
 }
 float get_humidity()
@@ -161,25 +162,26 @@ int get_ls_state()
 }
 int get_flow_info()
 {
-  if ((millis() - oldtime > 1000))
+  if ((millis() - old_flow_time > 1000))
   {
     detachInterrupt(sensor_interrupt);
-    flowrate = ((1000.0 / (millis() - oldtime)) * pulsecount) / calibration;
-    oldtime = millis();
-    volumeml = (flowrate / 60) * 1000;
-    totalml += volumeml;
+    flow_rate_ml = ((1000.0 / (millis() - old_flow_time)) * flow_sensor_pulse_count) / flow_sensor_tick_rate;
+    old_flow_time = millis();
+    flow_volume_ml = (flow_rate_ml / 60) * 1000;
+    flow_total_ml += flow_volume_ml;
     Serial.print("Flow Rate:");
-    Serial.print(flowrate);
+    Serial.print(flow_rate_ml);
     Serial.print("mL/min\n");
     Serial.print("Output Liquid Quantity: ");
-    Serial.print(totalml);
+    Serial.print(flow_total_ml);
     Serial.print("mL");
     Serial.print("\t"); // Print tab space
-    Serial.print(totalml / 1000);
+    long totallitres = flow_total_ml / 1000;
+    Serial.print(totallitres);
     Serial.print("L");
     Serial.print("\n");
-    pulsecount = 0;
-    attachInterrupt(sensor_interrupt, pulseCounter, FALLING);
+    flow_sensor_pulse_count = 0;
+    attachInterrupt(sensor_interrupt, flow_sensor_pulse_counter, FALLING);
   }
   else
   {
@@ -199,13 +201,13 @@ void turn_off_fwf_pump()
 int package_bundle()
 {
   get_flow_info();
-  arr[0] = convert_float_to_string(get_humidity());
-  arr[1] = convert_float_to_string(get_temperature());
-  arr[2] = convert_float_to_string(flowrate);
-  arr[3] = convert_float_to_string(totalml);
-  arr[4] = convert_float_to_string(volumeml);
+  arr[0] = convert_float_to_string(get_humidity(), 3, 2);
+  arr[1] = convert_float_to_string(get_temperature(), 3, 2);
+  arr[2] = convert_float_to_string(flow_rate_ml, 7, 2);
+  arr[3] = convert_float_to_string(flow_total_ml, 7, 2);
+  arr[4] = convert_float_to_string(flow_volume_ml, 7, 2);
   int n = sprintf(sender,
-                  "Humidity:%s#\nTemperature:%s#\nFlow_Frequency:%s#\nFlow_totalml:%s#\nFlow_volumeml:%s#\nHall_Effect_State:%d#\nLevel_Switch_State:%i#\nUV_State %d#\n",
+                  "Humidity:%s#\nTemperature:%s#\nFlow_Frequency:%s#\nFlow_flow_total_ml:%s#\nFlow_Rate_ml:%s#\nHall_Effect_State:%d#\nLevel_Switch_State:%i#\nUV_State %d#\n",
                   arr[0], arr[1], arr[2], arr[3], arr[4], get_he_state(), get_ls_state(), get_uv_state());
   Serial.println(sender);
   if (n < 0)
@@ -244,7 +246,7 @@ int recieve_data()
     }
   }
   else
-    Serial.println(n);
+    Serial.println("----No Bluetooth connection----");
   return -12;
 }
 int state_check(int phonedata)
@@ -304,3 +306,4 @@ int inboard_error(int code)
     return -53;
   }
 }
+
