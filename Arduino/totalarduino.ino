7fb7b6cfc764f1d11dab6aa864b79aa2c7a3a481
Lens Desrouleaux <65369396+lrdesrouleaux@users.noreply.github.com>
2021-03-24 15:44:24 -0400

    Update totalarduino.ino
    reverted to previous state

--- Arduino/totalarduino.ino
+++ Arduino/totalarduino.ino
@@ -1,5 +1,4 @@
 
-
 //level switch high signal, shut down fresh water pump and turn of uv bar
 //level switch low turn on uv sterilization and fresh water pump
 #include <dht.h> //Library needed for humidity and temperature sensors
@@ -9,51 +8,47 @@
 #define he_pin 25
 #define dht_pin 4
 #define fs_pin 2
-#define sensor_interrupt 0
+#define sensor_interrupt 0 
 #define ls_pin 20
 #define led_pin 53
 #define fwfp_pin 22
 //defining device pins
-#define bt Serial1                       //HC-05 bluetooth chip
-#define esp_module Serial2               //ESP module \
-                                         //all the information relating to the flow sensors
+#define bt Serial1                       //HC-05 bluetooth chip 
+                   //all the information relating to the flow sensors
 float flow_rate_ml;                      //keeps track of flow rate
 float flow_volume_ml;                    //keeps track of volume
 unsigned long flow_total_ml;             //keeps track of the total amount of flow
 unsigned long old_flow_time;             //keeps track of time passed for the flow function
 volatile byte flow_sensor_pulse_count;   //keeps track of the pulses the flow sensor has sent
-const float flow_sensor_tick_rate = 1;   //flow_sensor_tick_rate on the flow sensor
-const long recieve_time_trigger = 3000;  //triggers after 1000 ms to run code
-const long package_time_trigger = 3200;  //triggers after 4000 ms to run code
-unsigned long recieve_previous_time = 0; //keeps time before the recieve_bt_data code runs
+float flow_sensor_tick_rate = 1;         //flow_sensor_tick_rate on the flow sensor
+const long recieve_time_trigger = 1000;  //triggers after 1000 ms to run code
+const long package_time_trigger = 4000;  //triggers after 4000 ms to run code
+unsigned long recieve_previous_time = 0; //keeps time before the recieve_data code runs
 unsigned long package_previous_time = 0; //keeps time before the package and send code runs
 dht DHT;                                 //make a dht sensor object to read humidity and temperature
 int bt_error(int);                       //error handlers
 int inboard_error(int);
-int internet_error(int);
 int get_flow_info();                  //get all the information regarding the  flow
-int package_internet_bundle();        //builds the query string that will be elevated to the esp module
-int ascend_internet_bundle();         //send the internet bundle to the esp module
-int recieve_internet_data();          //read the bundle from the espmodule
+int package_bundle();                 // gather data for and formate the bublde string return negative if error
+int send_bundle();                    //send the bundle over the bt connection return negative if error
+int recieve_data();                   //read the bundle from the bt connection into the reader, return negative if error
 int get_he_state();                   //get hall effect state and return
 int get_ls_state();                   //get level switch state and return
 void turn_off_UV();                   //turns off the uv light does not return a value because the call to the pin is void
 void turn_on_UV();                    //turns on the uv light does not return a value because the call to the pin is void
 int state_check(int);                 //state handler for recieved data
 float get_humidity();                 //read humidity and return
-float get_temperature();            //read temperature and return
+float get_temperature();              //read temperature and return
 void turn_on_fwf_pump();              //turns on the fresh water fill pump
 void turn_off_fwf_pump();             //turns off the freshwater fill pump
 char *convert_float_to_string(float); //converts a float to a string
 void flow_sensor_pulse_counter();     //used to count pulses, keeps time for flowinfo
+char sender[255];                     // string to be formatted and sent over bluetooth
 int receiver = 0;                     //int that gets read from the phone
-int internet_reciever = 0;            //int that gets read from the esp module
-char query_string[255];               //string that holds the post data
-String holder;
-char *format_array[5];                //string array for formatting sender
+char *arr[5];                         //string array for formatting sender
 void setup()
 {
-  pinMode(bt_state, INPUT);
+  pinMode(bt_state,INPUT);
   pinMode(led_pin, OUTPUT);
   pinMode(ls_pin, INPUT_PULLUP);
   pinMode(he_pin, INPUT);
@@ -61,7 +56,6 @@ void setup()
   pinMode(fs_pin, INPUT);
   Serial.begin(9600);
   bt.begin(9600);
-  esp_module.begin(9600);
   flow_rate_ml = 0;
   flow_volume_ml = 0;
   flow_total_ml = 0;
@@ -72,36 +66,36 @@ void setup()
 
 void loop()
 {
-  int bt_packagecheck, bt_sendcheck, bt_readcheck;
-  int internet_packagecheck, internet_sendcheck, internet_readcheck;
+  int packagecheck, sendcheck, readcheck;
   unsigned long current_time = millis();
   //recieve data timer
-
   if (current_time - recieve_previous_time >= recieve_time_trigger)
   {
-    internet_readcheck = recieve_internet_data();
-    if (internet_readcheck < 0)
+    readcheck = recieve_data();
+    if (readcheck < 0)
     {
-      internet_error(internet_readcheck);
-      inboard_error(internet_readcheck);
+      bt_error(readcheck);
+      inboard_error(readcheck);
     }
-    state_check(internet_readcheck);
+    else
+    {
+      state_check(readcheck);
+    }
     recieve_previous_time = current_time;
   }
   //package and send timer
   if (current_time - package_previous_time >= package_time_trigger)
   {
-    Serial.println("Hello package");
-    internet_packagecheck = package_internet_bundle();
-    if (internet_packagecheck < 0)
+    if ((packagecheck = package_bundle()) >= 0)
     {
-      internet_error(internet_packagecheck);
-      inboard_error(internet_packagecheck);
-    }
-    ascend_internet_bundle();
+      Serial.println("----Successful Packaging----");
+      if ((sendcheck = send_bundle()) > 0)
+      {
+        Serial.println("----Successful Sending----");
+      } //sendcheck if
+    }   //packagecheck if
     package_previous_time = current_time;
-  }
-
+  } //package timer
 } //eof
 void flow_sensor_pulse_counter()
 {
@@ -109,8 +103,8 @@ void flow_sensor_pulse_counter()
 }
 char *convert_float_to_string(float n, int stringlength, int precision)
 {
-  char buff[16];
-  char *float_to_be_converted = buff;
+  char buff[stringlength];
+  char* float_to_be_converted = buff;
   dtostrf(n, stringlength, precision, float_to_be_converted);
   return float_to_be_converted;
 }
@@ -149,7 +143,7 @@ void turn_off_UV()
 }
 void turn_on_UV()
 {
-  Serial.println("Turning on UV light");
+  Serial.println("Turning off UV light");
   digitalWrite(uv_pin, LOW);
 }
 int get_uv_state()
@@ -205,12 +199,68 @@ void turn_off_fwf_pump()
 {
   digitalWrite(fwfp_pin, LOW);
 }
-
-int state_check(int data)
+int package_bundle()
 {
-  Serial.println("state check");
-  Serial.println(data);
-  switch (data)
+  get_flow_info();
+  arr[0] = convert_float_to_string(get_humidity(), 3, 2);
+  arr[1] = convert_float_to_string(get_temperature(), 3, 2);
+  arr[2] = convert_float_to_string(flow_rate_ml, 3, 2);
+  arr[3] = convert_float_to_string(flow_total_ml, 3, 2);
+  arr[4] = convert_float_to_string(flow_volume_ml, 3, 2);
+  int n = sprintf(sender,
+                  "Humidity:%s#\nTemperature:%s#\nFlow_Frequency:%s#\nFlow_flow_total_ml:%s#\nFlow_Rate_ml:%s#\nHall_Effect_State:%d#\nLevel_Switch_State:%i#\nUV_State %d#\n",
+                  arr[0], arr[1], arr[2], arr[3], arr[4], get_he_state(), get_ls_state(), get_uv_state());
+  Serial.println(sender);
+  if (n < 0)
+  {
+    return -14;
+  }
+  else
+    return 0;
+}
+int send_bundle()
+{
+  int btcheck=digitalRead(bt_state);
+  if(btcheck==LOW){
+    Serial.println("----No bluetooth connection aborting data transmission----");
+    return 0;
+    }
+  if (bt.availableForWrite())
+  {
+    Serial.println("Sending String:");
+    Serial.print(sender);
+    bt.println(sender);
+    return 1;
+  }
+  else
+    return -15;
+}
+int recieve_data()
+{
+   int btcheck=digitalRead(bt_state);
+  if(btcheck==LOW){
+    Serial.println("----No bluetooth connection aborting data transcription----");
+    return 0;
+    }
+  int num = receiver;
+  if (bt.available())
+  {
+    receiver = bt.read();
+    if (num != receiver)
+    {
+      Serial.println(receiver);
+      return receiver;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  
+}
+int state_check(int phonedata)
+{
+  switch (phonedata)
   {
   case 112:
     turn_on_UV();
@@ -233,7 +283,21 @@ int state_check(int data)
     break;
   }
 }
-
+int bt_error(int code)
+{
+  char message[21];
+  sprintf(message, "Error code %i#", code);
+  if (bt.availableForWrite() > 0)
+  {
+    bt.println(message);
+    return 0;
+  }
+  else
+  {
+    Serial.println("Error with the error handler sending over bluetooth");
+    return -52;
+  }
+}
 int inboard_error(int code)
 {
   char message[21];
@@ -250,61 +314,4 @@ int inboard_error(int code)
     Serial.println(message);
     return -53;
   }
-}
-int package_internet_bundle()
-{
-  Serial.println("internet2");
-  get_flow_info();
-  format_array[0] = convert_float_to_string(get_temperature(), 5, 2);
-  format_array[1] = convert_float_to_string(get_humidity(), 5, 2);
-  format_array[2] = convert_float_to_string(flow_rate_ml, 5, 2);
-  format_array[3] = convert_float_to_string(flow_total_ml, 5, 2);
-  format_array[4] = convert_float_to_string(flow_volume_ml, 5, 2);
-  int n = sprintf(query_string,
-                  "!temperature=%s&humidity=%s&flow_frequency=%s&flow_total_ml=%s&flow_rate_ml=%s&hall_effect_state=%i&level_switch_state=%i&uv_state=%i\r\n",
-                  format_array[0], format_array[1], format_array[2], format_array[3], format_array[4], get_he_state(), get_ls_state(), get_uv_state());
-  Serial.println(query_string);
-  if (n < 0)
-  {
-    return -14;
-  }
-  else
-    return 0;
-}
-
-int ascend_internet_bundle()
-{
-  Serial.println("5internet3");
-  Serial.println("Sending String to esp module:");
-  Serial.println(query_string);
-  String holder(query_string);
-  holder.replace(" ","");
-  holder.toLowerCase();
-  Serial.println(holder);
-  esp_module.println(holder);
-  return 2;
-}
-int internet_error(int code)
-{
-  char message[21];
-  sprintf(message, "Error code with ESP module %i#", code);
-  if (esp_module.availableForWrite())
-  {
-    esp_module.println(message);
-    return 0;
-  }
-  else
-  {
-    Serial.println("Error with the error handler ascending data to esp module");
-    return -54;
-  }
-}
-int recieve_internet_data()
-{
-  String ignore = Serial2.readStringUntil('#');
-  int response = Serial2.parseInt();
-  Serial.println("RESPONSE");
-  Serial.println(response);
-  Serial.println(ignore);
-  return response;
 }
